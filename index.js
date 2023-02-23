fa0cd4adfc826bd5e6f45472f9bf07a93bd2f3f3
v.starynets <v.starynets@sharksw.com>
2023-02-23 13:28:10 +0200

    added default port

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ const PORT = process.env.PORT || 5500;
 const app = express();
 app.use(cors());
 
-app.use(express.json({ extended: true }));
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({extended: true}));
+app.use(express.urlencoded({extended: true}))
 
 app.use("/api/currency", require("./routes/currency.route"));
 
 app.listen(PORT, () => {
     console.log(`App has been started on port ${PORT}`);
+});
+
+app.get("*", function (req, res) {
+    res.send('PAGE NOT FOUND');
 });
