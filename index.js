a0d20f078b2e0299c03b6b36954073c8d29dff00
v.starynets <v.starynets@sharksw.com>
2023-02-23 13:17:25 +0200

    changed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require('cors');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 
 const app = express();
 app.use(cors());
